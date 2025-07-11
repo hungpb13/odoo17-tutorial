7b08c5456c254a475bd3e71283967102dbf0026f
hungpb13 <hungpb13@gmail.com>
2025-07-11 17:02:44 +0700

    Add Update FBT để cập nhật không cần chờ cron

--- addons/website_frequently_bought_together/models/website_sale_product.py
+++ addons/website_frequently_bought_together/models/website_sale_product.py
@@ -5,21 +5,11 @@ class ProductTemplate(models.Model):
 
     def get_fbt_products(self):
         self.ensure_one()
-        manual_rule = self.env['website.fbt.rule'].with_context(prefetch_fields=False).search([
+        rule = self.env['website.fbt.rule'].with_context(prefetch_fields=False).search([
             ('product_id', '=', self.id),
-            ('active', '=', True),
-            ('is_manual', '=', True)
+            ('active', '=', True)
         ], limit=1)
-        ai_rule = self.env['website.fbt.rule'].with_context(prefetch_fields=False).search([
-            ('product_id', '=', self.id),
-            ('active', '=', True),
-            ('is_manual', '=', False)
-        ], limit=1)
-        product_ids = set()
-        if manual_rule:
-            product_ids.update(manual_rule.fbt_product_ids.ids)
-        if ai_rule:
-            product_ids.update(ai_rule.fbt_product_ids.ids)
+        product_ids = set(rule.fbt_product_ids.ids)
         # Chỉ lấy sản phẩm đang publish trên website
         products = self.env['product.template'].browse(list(product_ids))
         products = products.filtered(lambda p: p.website_published)
