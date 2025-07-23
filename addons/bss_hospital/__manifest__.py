e887ad2aa3e0ca5fe43e97ef8105ed76f0e566e6
hungpb13 <hungpb13@gmail.com>
2025-07-23 11:41:26 +0700

    Tracking In Odoo

--- addons/bss_hospital/__manifest__.py
+++ addons/bss_hospital/__manifest__.py
@@ -3,6 +3,9 @@
     "author": "BeeSmart Solutions",
     "license": "LGPL-3",
     "version": "17.0.1.0.0",
+    "depends": [
+        "mail",
+    ],
     "data": [
         "security/ir.model.access.csv",
         "views/patient_view.xml",
--- addons/bss_hospital/models/patient.py
+++ addons/bss_hospital/models/patient.py
@@ -4,8 +4,9 @@ from odoo import models, fields
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
+    _inherit = ['mail.thread']
 
-    name = fields.Char(string='Name', required=True)
-    date_of_birth = fields.Date(string='Date of Birth')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
+    name = fields.Char(string='Name', required=True, tracking=True)
+    date_of_birth = fields.Date(string='Date of Birth', tracking=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True)
     
