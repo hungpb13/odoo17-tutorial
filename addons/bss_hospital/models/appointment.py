de3a2105b0aca9f415951802009c11a87bb96146
hungpb13 <hungpb13@gmail.com>
2025-07-23 15:25:04 +0700

    Tutorials: Creating Statubar, Buttons, Conditional Invisible And Self In Odoo

--- addons/bss_hospital/models/appointment.py
+++ addons/bss_hospital/models/appointment.py
@@ -11,6 +11,13 @@ class HospitalAppointment(models.Model):
     patient_id = fields.Many2one('hospital.patient', string='Patient')
     date = fields.Date(string='Appointment Date')
     note = fields.Text(string='Notes')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirmed', 'Confirmed'),
+        ('ongoing', 'Ongoing'),
+        ('done', 'Done'),
+        ('cancelled', 'Cancelled')
+    ], string='Status', default='draft', tracking=True)
 
     @api.model_create_multi
     def create(self, val_list):
@@ -19,4 +26,18 @@ class HospitalAppointment(models.Model):
                 vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment')
         return super().create(val_list)
 
+    def action_confirm(self):
+        for record in self:
+            record.state = 'confirmed'
     
+    def action_ongoing(self):
+        for record in self:
+            record.state = 'ongoing'
+
+    def action_done(self):
+        for record in self:
+            record.state = 'done'
+
+    def action_cancel(self):
+        for record in self:
+            record.state = 'cancelled'
